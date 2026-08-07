35791fdb72779f1227348d426a18e711faf3d10d
BrianSHDZ <bh24286@gmail.com>
2026-08-06 19:25:27 -0600

    feat: se agregaron las excepciones para rol

---
+++ src/main/java/com/taskflow/taskflow/exception/RolNotFoundException.java
@@ -0,0 +1,7 @@
+package com.taskflow.taskflow.exception;
+
+public class RolNotFoundException extends RuntimeException{
+    public RolNotFoundException(String message){
+        super(message);
+    }
+}
--- src/main/java/com/taskflow/taskflow/service/RolServiceImp.java
+++ src/main/java/com/taskflow/taskflow/service/RolServiceImp.java
@@ -1,9 +1,11 @@
 package com.taskflow.taskflow.service;
 
 import com.taskflow.taskflow.entity.Rol;
+import com.taskflow.taskflow.exception.RolNotFoundException;
 import com.taskflow.taskflow.repository.IRolRepository;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.RoleNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,8 +23,15 @@ public class RolServiceImp implements IRolService{
     public Optional<Rol> obtenerRolPorId(Integer id) { return rolRepository.findById(id); }*/
 
     @Override
-    public Rol guardarRol(Rol rol) { return rolRepository.save(rol); }
+    public Rol guardarRol(Rol rol) {
+        if(rolRepository.findByNombre(rol.getNombre()).isPresent()){
+          throw new IllegalArgumentException("El rol" + rol.getNombre() + " ya existe en la base de datos");
+        } return rolRepository.save(rol);
+    }
 
     @Override
-    public void eliminarRol(Integer id) { rolRepository.deleteById(id); }
+    public void eliminarRol(Integer id) {
+        if(!rolRepository.existsById(id)){
+            throw new RolNotFoundException("El rol con ID" +id +"no existe");
+        }rolRepository.deleteById(id); }
 }
